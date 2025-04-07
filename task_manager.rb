5f3b4655d5da1f280d0c19a4ce82c035935928c7
chibrik0208 <ilya.ismailau@gmail.com>
2025-04-07 20:41:30 +0200

    Add task deletion by index

--- task_manager.rb
+++ task_manager.rb
@@ -14,4 +14,28 @@ class TaskManager
       puts "#{index + 1}. #{task}"
     end
   end
+
+  def search(keyword)
+    puts "\nSearch results for '#{keyword}':"
+
+    results = @tasks.select do |task|
+      task.title.downcase.include?(keyword.downcase) ||
+      task.description.downcase.include?(keyword.downcase)
+    end
+
+    if results.empty?
+      puts "No tasks found."
+    else
+      results.each { |task| puts task }
+    end
+  end
+
+  def delete_task(index)
+    if index.between?(1, @tasks.length)
+      deleted = @tasks.delete_at(index - 1)
+      puts "Deleted: #{deleted.title}"
+    else
+      puts "Invalid task number"
+    end
+  end
 end
